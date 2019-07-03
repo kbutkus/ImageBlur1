2aaf494b8ace9a27752b6dcad033be2a4546175d
kbutkus@gmail.com <kbutkus@gmail.com>
2019-07-03 06:11:39 +0000

    picture

--- picture.rb
+++ picture.rb
@@ -1,23 +1,21 @@
 class Image
-attr_accessor :array
 
   def initialize (array)
     @array = array
   end
 
-  def outout_image
-    @array.each do |row|
-      puts row.join
+  def output_image
+    @array.each do |data|
+      puts data.join
     end
   end
-
 end
 
-image = Image.new ([
-[0,0,0,0]
-[0,1,0,0]
-[0,0,0,1]
-[0,0,0,0]
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
 ])
 
-image.outout_image
+image.output_image
